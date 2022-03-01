4ba36b0d7907e5121fc7ac2901082b80fb95f93f
Alan <my_ant1@naver.com>
2022-03-01 02:18:08 -0500

    Basic stamina bar added with basic wall climbing, need more directional movements

--- rtr/Assets/Scripts/PlayerController.cs
+++ rtr/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	// player variables
-	private float stamina = 100f;
-	private float maxStamina = 100f;
-	private float speed = 10f;
+	private float stamina = 1f;
+	private float maxStamina = 1f;
+	private bool outOfStamina = false;
+
+	private float speed = 50f;
 	private float moveSmoothing = 0.05f;
-	private float jumpForce = 300f;
+	private float jumpForce = 500f;
 	private float horizontalMove = 0.0f;
 
 	private Vector3 zeroVel = Vector3.zero;
@@ -19,33 +21,77 @@ public class PlayerController : MonoBehaviour
 	private bool canJump = false;
 	private bool lookingRight = true;
 
+	private bool onWall = false;
+
+	[SerializeField] private StaminaBar staminaBar;
+
+	private float gravityScale = 1f;
 	private Rigidbody2D playerRigidbody;
 	private Animator playerAnimator;
 	private SpriteRenderer playerRenderer;
 
 	private void Start()
 	{
+		staminaBar.SetSize(stamina);
+
 		playerRigidbody = GetComponent<Rigidbody2D>();
 		playerAnimator = GetComponent<Animator>();
 		playerRenderer = GetComponent<SpriteRenderer>();
+
+		playerRigidbody.gravityScale = gravityScale;
 	}
 
     private void Update()
     {
 		horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
 
-		if (Input.GetButtonDown("Jump") && jumped == false)
+		if (stamina != 0 && Input.GetButtonDown("Jump") && jumped == false)
 		{
 			jumped = true;
+			onWall = false;
+
+			playerRigidbody.gravityScale = gravityScale;
 			//playerAnimator.SetBool("isOnGround", false);
 			//AudioManager.instance.PlayOneShot(jumpSound);
+		}
+		if (stamina != 0 && Input.GetKeyDown(KeyCode.Q) && !onWall)
+        {
+			onWall = true;
+			jumped = false;
+			canJump = true;
+
+			playerRigidbody.velocity = Vector2.zero;
+			playerRigidbody.gravityScale = 0f;
+        }
+		if (Input.GetKeyDown(KeyCode.E) && onWall)
+		{
+			onWall = false;
+
+			playerRigidbody.gravityScale = gravityScale;
 		}
 	}
 
     private void FixedUpdate()
 	{
+		if (outOfStamina)
+        {
+			onWall = false;
+			playerRigidbody.gravityScale = gravityScale;
+        }
+
 		Move(horizontalMove * Time.fixedDeltaTime, false, jumped);
 	}
+
+	private void UpdateStamina(float c)
+    {
+		stamina = c;
+		if (stamina < 0)
+        {
+			stamina = 0;
+			outOfStamina = true;
+        }
+		staminaBar.SetSize(stamina);
+    }
 
 
 	public void Move(float move, bool crouch, bool jump)
@@ -55,16 +101,16 @@ public class PlayerController : MonoBehaviour
 		// And then smoothing it out and applying it to the character
 		playerRigidbody.velocity = Vector3.SmoothDamp(playerRigidbody.velocity, targetVelocity, ref zeroVel, moveSmoothing);
 
+		if (onWall && move > 0) UpdateStamina(stamina - 0.01f);
+
 		// If the input is moving the player right and the player is facing left...
 		if (move > 0 && !lookingRight)
 		{
-			// ... flip the player.
 			Flip();
 		}
 		// Otherwise if the input is moving the player left and the player is facing right...
 		else if (move < 0 && lookingRight)
 		{
-			// ... flip the player.
 			Flip();
 		}
 
@@ -76,7 +122,7 @@ public class PlayerController : MonoBehaviour
 			onGround = false;
 			canJump = false;
 
-			stamina -= 50;
+			UpdateStamina(stamina - 0.4f);
 
 			playerRigidbody.AddForce(new Vector2(0f, jumpForce));
 		}
@@ -100,9 +146,11 @@ public class PlayerController : MonoBehaviour
         {
 			onGround = true;
 			canJump = true;
+			onWall = false;
+			outOfStamina = false;
 
 			// reset max stamina
-			stamina = maxStamina;
-        }
+			UpdateStamina(maxStamina);
+		}
     }
 }
---
+++ rtr/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaBar : MonoBehaviour
+{
+    private Transform bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(1f, sizeNormalized);
+    }
+}
