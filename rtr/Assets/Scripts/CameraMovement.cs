500808f2a1c55a6d27c480d63a0786788e2aabc8
npackard <nicolespackard@gmail.com>
2022-03-01 20:46:52 -0500

    camera shows goal and returns to rat

--- rtr/Assets/Scripts/CameraMovement.cs
+++ rtr/Assets/Scripts/CameraMovement.cs
@@ -8,19 +8,36 @@ public class CameraMovement : MonoBehaviour
     public Transform playerTransform;
     public float dampTime = 0.1f;
 
+    public GameObject goal;
+
+    private bool cutSceneDone = false;
 
     private Vector3 velocity = Vector3.zero;
 
     private void Start()
     {
-        
+        StartCoroutine(DoCutscene());
     }
 
     private void Update()
     {
-        Vector3 delta = new Vector3(playerTransform.position.x, 0, -10) - cam.ViewportToWorldPoint(new Vector2(0.5f, 0.5f));
-        Vector3 destination = transform.position + delta;
-        if (destination.x < 0) destination = new Vector3(0, destination.y, destination.z);
-        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        if (cutSceneDone) {
+            Vector3 delta = new Vector3(playerTransform.position.x, 0, -10) - cam.ViewportToWorldPoint(new Vector2(0.5f, 0.5f));
+            Vector3 destination = transform.position + delta;
+            if (destination.x < 0) destination = new Vector3(0, destination.y, destination.z);
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+    }
+
+    private IEnumerator DoCutscene() {
+        float xDist = (Mathf.Abs(goal.transform.position.x - playerTransform.position.x) / 10) - .5f;
+        float yDist = (Mathf.Abs(goal.transform.position.y - playerTransform.position.y) / 10) + .5f;
+        for (int i = 0; i < 275; i++) {
+            Vector3 goalPos = new Vector3(transform.position.x + xDist, transform.position.y + yDist, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, dampTime);
+            yield return new WaitForSeconds(.01f);
+        }
+        yield return new WaitForSeconds(1f);
+        cutSceneDone = true;
     }
 }
