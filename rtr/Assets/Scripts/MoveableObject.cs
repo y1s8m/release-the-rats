dc4abde620d82f372a3d7cf959bcee6678fbac80
npackard <nicolespackard@gmail.com>
2022-03-22 18:56:46 -0400

    moveable objects rigidbodies

--- rtr/Assets/Scripts/MoveableObject.cs
+++ rtr/Assets/Scripts/MoveableObject.cs
@@ -8,10 +8,13 @@ public class MoveableObject : MonoBehaviour
 
     private bool held = false;
 
+    private Rigidbody2D rb;
+    private Vector3 zeroVec = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,8 +24,10 @@ public class MoveableObject : MonoBehaviour
             if (!PlayerController.instance.GetGrabbing()) {
                 this.gameObject.transform.parent = null;
                 held = false;
+                Reset();
             } else {
                 this.gameObject.transform.parent = player;
+                Hold();
             }
         }
     }
@@ -32,6 +37,33 @@ public class MoveableObject : MonoBehaviour
             // grab object
             this.gameObject.transform.parent = player;
             held = true;
+            Hold();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player" && PlayerController.instance.GetGrabbing()) {
+            // grab object
+            this.gameObject.transform.parent = player;
+            held = true;
+            Hold();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            Reset();
+        }
+    }
+
+    private void Reset() {
+        if (rb == null) rb = gameObject.AddComponent<Rigidbody2D>();
+    }
+
+    private void Hold() {
+        if (rb != null) {
+            Destroy(rb);
+            Debug.Log("gone!");
         }
     }
 }
