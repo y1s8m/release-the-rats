a258333421d23e2bd50b9fe2c57fc671739747a5
y1s8m <ysm180701@gmail.com>
2022-03-29 20:09:15 -0400

    Maze1 fixes wip

--- rtr/Assets/MazeCurve.cs
+++ rtr/Assets/MazeCurve.cs
@@ -5,32 +5,7 @@ using UnityEngine;
 public class MazeCurve : MonoBehaviour
 {
     public bool collided = false;
-    public bool pcollided = false;
-    public bool isChild = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Transform nextNode;
 
-    void OnTriggerEnter2D(Collider2D col){
-        if (col.gameObject.tag == "Player"){
-            collided = true;
-            col.gameObject.GetComponent<MazeMovement>().isChild = true;
-            if (this.gameObject.transform.parent.GetComponent<MazeCurveParent>().collided){
-                collided = false;
-                this.gameObject.transform.parent.GetComponent<MazeCurveParent>().collided = false;
-                pcollided = true; 
-            }
-            else {
-                pcollided = false;
-            }
-        }
-    }
 }
--- rtr/Assets/MazeCurveParent.cs
+++ rtr/Assets/MazeCurveParent.cs
@@ -5,35 +5,6 @@ using UnityEngine;
 public class MazeCurveParent : MonoBehaviour
 {
     public bool collided = false;
-    public bool chcollided = false;
-    public bool isChild = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void OnTriggerEnter2D(Collider2D col){
-        if (col.gameObject.tag == "Player"){
-            collided = true;
-            col.gameObject.transform.parent.GetComponent<MazeMovement>().isChild = false;
-            if (this.gameObject.transform.GetChild(0).gameObject.GetComponent<MazeCurve>().collided){
-                collided = false;
-                this.gameObject.transform.GetChild(0).gameObject.GetComponent<MazeCurve>().collided = false;
-                //move player 
-                chcollided = true;
-                //move player
-            }
-            else {
-                //col.gameObject.transform.position =  this.gameObject.transform.GetChild(0).gameObject.transform.position;
-                chcollided = false;
-                //col.gameObject.transform.position =  this.gameObject.transform.GetChild(0).gameObject.transform.position;
-            }
-        }
-    }
+    public Transform nextNode;
 }
